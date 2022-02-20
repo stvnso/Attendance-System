fd9b3939ba9c2ffa0f2f5c8e11e641292c3d6716
stvnso <stevensowa2910@gmail.com>
2022-02-20 16:49:58 +0100

    dbchanges

---
+++ ExampleCode/DB/Check_Out_Query.sql
@@ -0,0 +1,9 @@
+UPDATE attendance
+set 
+	checked_in = 0,
+    check_out_time = time("now", "localtime")
+where id = 2 //rfid
+
+UPDATE attendance
+SET working_time_account = round(working_time_account - (daily_working_hours - ((strftime('%s',[check_out_time]) - strftime('%s',[check_in_time])*1.0)/3600)),2)
+where rfid_uid
---
+++ ExampleCode/DB/Create_Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE attendance(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  rfid_uid INTEGER NOT NULL, 
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  access_level INTEGER DEFAULT 1 CHECK(access_level IN (1,3)),
+  checked_in INTEGER DEFAULT 0 CHECK(checked_in IN (0,1)),
+  date DATETIME DEFAULT (strftime('%d-%m-%Y', 'now', 'localtime')),
+  check_in_time TIME DEFAULT ,
+  check_out_time TIME ,
+  daily_working_hours real default 8.0,
+  working_time_account Real DEFAULT 0 );
