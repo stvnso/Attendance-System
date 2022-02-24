3536d76a9afc330cb02e77d2b07b5624dcca381b
stvnso <stevensowa2910@gmail.com>
2022-02-24 22:28:04 +0100

    programm changes

--- Attendance_functions.py
+++ Attendance_functions.py
@@ -1,6 +1,7 @@
 #imports
 
 #reader
+from re import S
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
@@ -28,22 +29,16 @@ display = drivers.Lcd()
 # DATABASE CONNECTION
 
 # connect to database
-database_Attendance = sqlite3.connect('database/attendance-system.db')
+# try:
+#         # Datenbankverbindung aufbauen + Cursor erstellen
+#         database_Attendance = sqlite3.connect('database/attendance-system.db')
+#         print("Database connection successfull")
 
+#         dbCursor = database_Attendance.cursor()
+#         print("Created Cursor for Database")
 
-dbCursor = database_Attendance.cursor()
-print("\nCreated Database Cursor \n")
-
-try:
-        # Datenbankverbindung aufbauen + Cursor erstellen
-        database_Attendance = sqlite3.connect('database/attendance-system.db')
-        print("Database connection successfull")
-
-        dbCursor = database_Attendance.cursor()
-        print("Created Cursor for Database")
-
-except sqlite3.Error as error:
-        print("Failed to connect to Database", error)
+# except sqlite3.Error as error:
+#         print("Failed to connect to Database", error)
 
 
 
@@ -52,11 +47,44 @@ except sqlite3.Error as error:
 
 # FUNCTIONS
 
+def execute_SQL_Query(sql_query,rfid_ID):
+    try:
+        # Datenbankverbindung aufbauen + Cursor erstellen
+        database_Attendance = sqlite3.connect('database/attendance-system.db')
+       # print("Database connection successfull")
+        dbCursor = database_Attendance.cursor()
 
-def close_db_connection():
+        dbCursor.execute(sql_query,(rfid_ID,))
+        database_Attendance.commit()
+
+        sql_query_result = dbCursor.fetchall()
+
+        return sql_query_result
+
+    except sqlite3.Error as error:
+        print("Failed to connect to Database", error)
 
 
-    database_Attendance.close()
+def create_db_connection():
+    try:
+        # Datenbankverbindung aufbauen + Cursor erstellen
+        database_Attendance = sqlite3.connect('database/attendance-system.db')
+        print("Database connection successfull")
+
+        return database_Attendance
+
+    except sqlite3.Error as error:
+        print("Failed to connect to Database", error)
+
+
+def create_db_cursor(database):
+    dbCursor = database.cursor()
+    return dbCursor
+
+
+def close_db_connection(database,dbCursor):
+
+    database.close()
     print("Closed Database connection")
 
     dbCursor.close()
@@ -80,77 +108,128 @@ def readRFID_fromChip():
             print("\n-------------------------------------")
             print("RFID_UID:\t" + rfid_ID)
             print("-------------------------------------\n")
+            display.lcd_clear()
     finally:
             GPIO.cleanup()
+
 
     return rfid_ID
 
 #-------------------------------------------------------------------
 
-def read_Entry_fromDatabase_by_RFID(rfid_ID):
+def read_user_fromDatabase_by_RFID(rfid_ID):
 
     #rfid_ID = readRFID_fromChip()
 
     # Datenbank SELECT Query mit gegebenem Parameter
-    dbCursor.execute(
-        "SELECT * FROM attendance WHERE rfid_uid=?", (rfid_ID,))
-    print("Executing SELECT-Query for RFID:\t" + rfid_ID)
+    sql_query = """SELECT * FROM attendance WHERE rfid_uid=?"""
 
     # Daten im Array speichern
-    selectedData = dbCursor.fetchall()
+    selectedData = execute_SQL_Query(sql_query,rfid_ID)
+
     print("Database Entry for RFID:\t" + rfid_ID)
     print("---------------------------")
 
     # Über Array iterieren und Einträge ausgeben
-    for entry in selectedData:
-        print("Id: \t\t\t", entry[0])
-        print("RFID_UID: \t\t", entry[1])
-        print("First-Name: \t\t", entry[2])
-        print("Last-Name: \t\t", entry[3])
-        print("Access-Level: \t\t", entry[4])
-        print("Checked-IN: \t\t", entry[5])
-        print("Date: \t\t\t", entry[6])
-        print("Check-In-Time: \t\t", entry[7])
-        print("Check-Out-Time: \t", entry[8])
-        print("Daily-Working-Hours: \t", entry[9])
-        print("Working-Time-Account: \t", entry[10])
+    for user_entry in selectedData:
+        print("Id: \t\t\t", user_entry[0])
+        print("RFID_UID: \t\t", user_entry[1])
+        print("First-Name: \t\t", user_entry[2])
+        print("Last-Name: \t\t", user_entry[3])
+        print("Access-Level: \t\t", user_entry[4])
+        print("Checked-IN: \t\t", user_entry[5])
+        print("Date: \t\t\t", user_entry[6])
+        print("Check-In-Time: \t\t", user_entry[7])
+        print("Check-Out-Time: \t", user_entry[8])
+        print("Daily-Working-Hours: \t", user_entry[9])
+        print("Working-Time-Account: \t", user_entry[10])
         print("---------------------------")
         print("\n")
 
-        return entry
+        return user_entry
 #-------------------------------------------------------------------
 
-def check_in():
+def is_user_checked_in(rfid_ID):
 
-    rfid_ID = readRFID_fromChip()
+    user = read_user_fromDatabase_by_RFID(rfid_ID)
+    checked_in = user[5]
 
-    sql_update_query =  """Update attendance 
-                            SET
-                            date = strftime('%d-%m-%Y', 'now', 'localtime'),
-                            check_in_time = time('now','localtime'),
-                            checked_in = 1
-                            where rfid_uid =?""" 
-    dbCursor.execute(sql_update_query,(rfid_ID,))
-    database_Attendance.commit()
+    if checked_in == 1:
+        checked_in_status = True
+    else:
+        checked_in_status = False
 
     
-    user = read_Entry_fromDatabase_by_RFID(rfid_ID)
+    print("User: " + user[2] + " " + user[3] + " has checked_in status: " + str(checked_in_status))
+    
+    return checked_in_status
+#-------------------------------------------------------------------
+
+def check_IN(rfid_ID):
+
+    #rfid_ID = readRFID_fromChip()
+
+    sql_query =  """Update attendance 
+                            SET
+                            date = strftime('%d-%m-%Y', 'now', 'localtime'),
+                            check_in_time = strftime('%H:%M', 'now', 'localtime'),
+                            checked_in = 1
+                            where rfid_uid =?""" 
+  
+    execute_SQL_Query(sql_query,rfid_ID)
+
+    
+    user = read_user_fromDatabase_by_RFID(rfid_ID)
 
     print("Successfully checked in user: " + user[2] + " " + user[3] )
 
     first_name  = user[2]
     last_name = user[3]
     check_in_time = user[7]
+    working_time_account = user[10]
 
     print("\nPrinting to Display..")
     display.lcd_display_string("Hi "  + first_name + " " + last_name,1)
     display.lcd_display_string("Kommen: "  + check_in_time,2)
     sleep(3)
     display.lcd_clear()
+    display.lcd_display_string("Arbeitszeitkonto: ",1)
+    display.lcd_display_string(str(working_time_account) + " h",2)
+    sleep(3)
+    display.lcd_clear()
 
-   
-#check_in()
+#-------------------------------------------------------------------
+
+def check_OUT(rfid_ID):
     
+    sql_query = """UPDATE attendance
+                    set 
+                    checked_in = 0,
+                    check_out_time = strftime('%H:%M', 'now', 'localtime'),
+                    working_time_account = round(working_time_account - (daily_working_hours - ((strftime('%s',[check_out_time]) - strftime('%s',[check_in_time])*1.0)/3600)),2)
+                    where rfid_uid =?;"""
+
+    execute_SQL_Query(sql_query,rfid_ID)
+
+    user = read_user_fromDatabase_by_RFID(rfid_ID)
+
+    print("Successfully checked out user: " + user[2] + " " + user[3] )
+
+    first_name  = user[2]
+    last_name = user[3]
+    check_out_time = user[8]
+    working_time_account = user[10]
+
+    print("\nPrinting to Display..")
+    display.lcd_display_string("Tschau "  + first_name + " " + last_name,1)
+    display.lcd_display_string("Gehen: "  + str(check_out_time),2)
+    sleep(3)
+    display.lcd_clear()
+
+    display.lcd_display_string("Arbeitszeitkonto: ",1)
+    display.lcd_display_string(str(working_time_account) + "h",2)
+    sleep(4)
+    display.lcd_clear()
 
 
 
--- Main.py
+++ Main.py
@@ -1,13 +1,30 @@
 #/usr/bin/env python
 
+from ast import While
 import RPi.GPIO as GPIO
+from numpy import False_
 import drivers
 from time import sleep
-import Attendance_functions
+from Attendance_functions import *
 
 
-    
-Attendance_functions.check_in()
+
+while True:
+    rfid_ID = readRFID_fromChip()
+
+    check_user = is_user_checked_in(rfid_ID)
+
+    if check_user == True:
+        check_OUT(rfid_ID)
+    elif check_user == False:
+        check_IN(rfid_ID)
+    else:
+        print("user nicht am start")
+
+
+
+
+
 
 
     
