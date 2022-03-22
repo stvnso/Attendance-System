a862b1f6fd55f8cdde50c62819b14929111a2c85
stvnso <stevensowa2910@gmail.com>
2022-03-22 20:16:24 +0100

    implement pir sensors

--- led-test.py
+++ led-test.py
@@ -6,28 +6,10 @@ from time import sleep
 
 display = lcd_drivers.Lcd()
 
-
-#button = Button(27)
-
-
 led = LED(12)
 
 
-btn = Button(27)
-
 led.on()
-display.lcd_display_string("Wait for press",1)
-
-btn.wait_for_press()
-
-display.lcd_clear()
-
-
-if btn.is_pressed:
-    print("button is pressed")
-    led.off()
-
-sleep(2)
-led.on()
-sleep(2)
+sleep(3)
 led.off()
+
