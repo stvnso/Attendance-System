4dcc52890f91e219b1f91e8e0243b7bd2e24a861
stvnso <stevensowa2910@gmail.com>
2022-03-27 17:12:37 +0200

    main attendance func

--- Attendance_System_Main.py
+++ Attendance_System_Main.py
@@ -1,29 +1,26 @@
 #/usr/bin/env python
 
-import sys
 from time import sleep
 from Attendance_System_Functions import *
 from Alarm_System_Main import *
 
-while True:
-    rfid_ID = readRFID_fromChip()
+def attendance_Main():
 
-    if does_user_exist(rfid_ID) == False:
-        continue
+    while True:
+        rfid_ID = readRFID_fromChip()
 
-    check_user = is_user_checked_in(rfid_ID)
+        if does_user_exist(rfid_ID) == False:
+            continue
 
-    if check_user == True:
-        check_OUT(rfid_ID)
-    elif check_user == False:
-        check_IN(rfid_ID)
-    
-    # if are_users_checked_in() == False:
-    #     print("Keiner mehr da")
+        check_user = is_user_checked_in(rfid_ID)
+
+        if check_user == True:
+            check_OUT(rfid_ID)
+        elif check_user == False:
+            check_IN(rfid_ID)
         
-    #     main_function()
-    #     exit()
 
+attendance_Main()
 
 
 
