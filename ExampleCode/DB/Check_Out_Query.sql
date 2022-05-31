c629068af6b44027d47ca35059e593ec6f297ada
stvnso <stevensowa2910@gmail.com>
2022-05-31 21:02:52 +0200

    function changes

--- ExampleCode/DB/Check_Out_Query.sql
+++ ExampleCode/DB/Check_Out_Query.sql
@@ -9,3 +9,10 @@ where id = 2
 UPDATE attendance
 SET working_time_account = round(working_time_account - (daily_working_hours - ((strftime('%s',[check_out_time]) - strftime('%s',[check_in_time])*1.0)/3600)),2)
 where rfid_uid
+
+
+UPDATE attendance
+set 
+	first_name = "Steven",
+    last_name = "Sowa"
+where first_name = "Eric"
